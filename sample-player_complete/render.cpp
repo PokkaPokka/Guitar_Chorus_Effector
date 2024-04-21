ecf3781eef54b90a73366d4fc66d2af36c0a4527
1111yuqi111 <166269431+1111yuqi111@users.noreply.github.com>
2024-04-21 00:14:41 -0400

    Update render.cpp
    add the led pin for digit pin 1 and digit pin 2

--- sample-player_complete/render.cpp
+++ sample-player_complete/render.cpp
@@ -4,17 +4,21 @@
 #include <libraries/GuiController/GuiController.h>
 #include "chorus.h"
 #include "debouncer.h"
+#include <libraries/Biquad/Biquad.h>
 
 #include <array>
 #include <memory>
 
-std::string gFilename = "guitarLoop.wav";
+std::string gFilename = "guitarLoop__1_.wav";
 std::vector<float> gSampleBuffer;
 int gReadPointer = 0;
 float gMixLevel = 0;
 const unsigned int kButtonPin = 0;
+const unsigned int kLedPin1 = 1;  // LED for voiceNum change
+const unsigned int kLedPin2 = 2;  // LED when voiceNum is 4
 int voiceNum = 4;
 Debouncer gDebouncer;
+float randomNum;
 
 // An array to store up to 14 Chorus instance for multi-voice chorus
 std::array<std::unique_ptr<Chorus>, 14> chorusEffect;
@@ -39,36 +43,33 @@ void render(BelaContext *context, void *userData) {
     for (unsigned int n = 0; n < context->audioFrames; n++) {
     	// Read from analog inputs
     	float mixLevel = map(analogRead(context, n/2, 0), 0, 3.3 / 4.096, 0, 100) * 0.01;
-		float baseDelayTime = map(analogRead(context, n/2, 1), 0, 3.3 / 4.096, 0.001, 0.049);
-		float lfoFrequency = map(analogRead(context, n/2, 2), 0, 3.3 / 4.096, 0.01, 0.5);
+		float baseDelayTime = map(analogRead(context, n/2, 1), 0, 3.3 / 4.096, 0.002, 0.004);
+		float lfoFrequency = map(analogRead(context, n/2, 2), 0, 3.3 / 4.096, 0.1, 0.8);
 		float voiceNumReciprocal = 1.0f / voiceNum;
 		int buttonValue = digitalRead(context, n, kButtonPin);
 		
 		// Debounce the button
 		gDebouncer.process(buttonValue);
+		
+		bool increased = false;
 		if( gDebouncer.fallingEdge() ) {
-			switch (voiceNum) {
-			    case 4:
-			        voiceNum += 4;
-			        break;
-			    case 8:
-			        voiceNum += 4;
-			        break;
-			    case 12:
-			        voiceNum += 2;
-			        break;
-			    case 14:
-			    	voiceNum = 4;
-			    	break;
-			    default:
-			        voiceNum = 4;
-			        break;
+			if (voiceNum >= 14) {
+				voiceNum = 4;
+				printf("voice num is 4");
+			} else {
+				voiceNum += 2;
+				increased = true;
+				printf("voice num +2");
 			}
     	}  
+    	
+    	digitalWrite(context, n, kLedPin1, increased ? HIGH : LOW);
+        digitalWrite(context, n, kLedPin2, voiceNum == 4 ? HIGH : LOW);
 		
 		for (int i = 0; i < voiceNum; i++) {
+			randomNum = (rand()%20)/50.0+0.01;
         	chorusEffect[i]->setBaseDelayTime(baseDelayTime);
-        	chorusEffect[i]->setLFOFrequency(lfoFrequency * (1 + 0.005 * i));
+        	chorusEffect[i]->setLFOFrequency(lfoFrequency * (1 + 0.05 * i));
     	}	
     	
         float mixedSample = 0;
@@ -82,6 +83,7 @@ void render(BelaContext *context, void *userData) {
         // Mix the original and processed samples
         float out = (1 - mixLevel) * originalSample 
                     + mixLevel * mixedSample * voiceNumReciprocal;
+                    
       
         for (unsigned int channel = 0; channel < context->audioOutChannels; channel++) {
             audioWrite(context, n, channel, out);
@@ -93,8 +95,10 @@ void render(BelaContext *context, void *userData) {
 }
 
 void cleanup(BelaContext *context, void *userData) {
-    // Clean up the chorus effect instance
+    // Clean up the chorus effect instance and turn off LEDs
     for(auto& chorus : chorusEffect) {
         chorus.reset();
     }
+    digitalWrite(context, 0, kLedPin1, LOW);
+    digitalWrite(context, 0, kLedPin2, LOW);
 }
